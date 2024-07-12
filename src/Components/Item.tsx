6fb12fa579abe6db7006595f1d783826de77d201
Hadis <hnoori214@gmail.com>
2024-07-12 14:55:32 +0200

    feat: add Item

--- src/Components/Item.tsx
+++ src/Components/Item.tsx
@@ -1,37 +1,65 @@
+import { useNavigate } from "react-router-dom";
 import "../Styles/Item.css";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { IconButton } from "@mui/material";
+interface DataItem {
+  id: number;
+  img: string;
+  title: string;
+  category: string;
+  price: number;
+  description: string;
+}
 type itemProps = {
+  id: number;
   img: string;
   title: string;
   category: string;
   price: number;
   description: string;
   showRemoveIcon?: boolean;
+  onDelete?: (index: number) => void;
+  data: DataItem[];
 };
 function Item({
+  id,
   img,
   title,
   category,
   price,
   description,
   showRemoveIcon,
+  onDelete,
+  data,
 }: itemProps) {
+  const navigate = useNavigate();
+  const handleClickItem = () => {
+    const foundItem = data.find((item) => item.id === id);
+    console.log(foundItem);
+    navigate(`/product/${id}`);
+  };
+
   return (
     <>
       <table className="content-item">
         <tr>
-          <td>
-            {" "}
+          <td onClick={handleClickItem}>
             <img src={img} alt={title} />
           </td>
-          <td>{title}</td>
-          <td>{category}</td>
-          <td>
+          <td onClick={handleClickItem}>{title}</td>
+          <td onClick={handleClickItem}>{category}</td>
+          <td onClick={handleClickItem}>
             {price}
             <span className="txt-color">$</span>
           </td>
-          <td>{description}</td>
-          <td>{showRemoveIcon && <DeleteOutlineIcon></DeleteOutlineIcon>}</td>
+          <td onClick={handleClickItem}>{description}</td>
+          <td>
+            {onDelete && (
+              <IconButton onClick={onDelete}>
+                <DeleteOutlineIcon></DeleteOutlineIcon>
+              </IconButton>
+            )}
+          </td>
         </tr>
       </table>
     </>
