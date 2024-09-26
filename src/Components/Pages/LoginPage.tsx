650736aaea584c9220867aeb5afb0aa29a2f6505
Hadis <hnoori214@gmail.com>
2024-09-26 16:27:03 +0200

    feat: add login page

--- src/Components/Pages/LoginPage.tsx
+++ src/Components/Pages/LoginPage.tsx
@@ -1,7 +1,11 @@
 import { Stack, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import "../../Styles/Login.css";
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
 function LoginPage() {
+  const [captchaValue, setCaptchaValue] = useState<string | null>(null);
   return (
     <div className="login">
       <form className="login-form">
@@ -19,7 +23,13 @@ function LoginPage() {
             label="Password"
             variant="outlined"
           />
-          <Button fullWidth variant="contained">
+          <ReCAPTCHA
+            sitekey="6LfJx08qAAAAAD6aNeqEAuMity3ptk3nYeocbegi"
+            onChange={(captchaValue: string | null) =>
+              setCaptchaValue(captchaValue)
+            }
+          />
+          <Button fullWidth variant="contained" disabled={!captchaValue}>
             Login
           </Button>
         </Stack>
