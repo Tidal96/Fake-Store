6fc9cc7a3a7cd086c519cc18942a1a253692e362
Hadis <hnoori214@gmail.com>
2024-09-26 16:31:23 +0200

    feat: rout to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { ThemeProvider } from "@mui/material";
 import "./App.css";
 import { createContext, useState } from "react";
-import Homepage from "./Components/Homepage";
+import Homepage from "./Components/Pages/Homepage";
 import { createTheme } from "@mui/material";
-export const appContext= createContext({darkMode:false, toggleDarkMode:()=>{}})
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/Pages/LoginPage";
+import Cart from "./Components/Pages/Cart";
+import Product from "./Components/Pages/Product";
+export const appContext = createContext({
+  darkMode: false,
+  toggleDarkMode: () => {},
+});
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const toggleDarkMode = () => {
@@ -12,15 +19,21 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: darkMode ? "dark" : "light",
+      primary: { main: "rgba(243, 157, 18, 0.76)" },
     },
   });
 
   return (
-    <appContext.Provider value={{darkMode,toggleDarkMode}}>
+    <appContext.Provider value={{ darkMode, toggleDarkMode }}>
       <ThemeProvider theme={theme}>
-        <Homepage>
-          
-        </Homepage>
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Homepage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="product/:productId" element={<Product />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </appContext.Provider>
   );
